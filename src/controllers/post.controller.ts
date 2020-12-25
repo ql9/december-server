3ebe999f3c53e0f0acd686201837281ad4590e46
Quốc Lê <ql9.lms@gmail.com>
2020-12-25 17:16:15 +0700

    fix like, unlike

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -160,7 +160,7 @@ export const like = async (req: Request, res: Response) => {
 };
 
 export const unLike = async (req: Request, res: Response) => {
-    const { postId, userId } = req.params;
+    const { postId, userId } = req.body;
     await Post.findById(postId)
         .then(async post => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
